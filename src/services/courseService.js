4ad6522e77b46b3973bcd493abd4959439071a33
Marcylene Barreto <marsouza93@gmail.com>
2024-08-31 12:31:06 -0300

    erro service

--- src/services/courseService.js
+++ src/services/courseService.js
@@ -2,32 +2,33 @@ const { ObjectId } = require('mongodb');
 const { getCollectionDB } = require('../data/db');
 
 const getAllCourses = async (dbClient) => {
-    const collection = getCollectionDB(req.dbClient, 'curso', 'modulo');
-    return await collection.find({}).toArray();
+    const collection = getCollectionDB(dbClient, 'curso', 'modulo');
+    const cursos = await collection.find({}).toArray();
+    return cursos;
 };
 
 const getCourseById = async (dbClient, id) => {
-    const collection = getCollectionDB(req.dbClient, 'curso', 'modulo');
+    const collection = getCollectionDB(dbClient, 'curso', 'modulo');
     return await collection.findOne({ _id: new ObjectId(id) });
 };
 
 const createCourse = async (dbClient, novoCurso) => {
-    const collection = getCollectionDB(req.dbClient, 'curso', 'modulo');
+    const collection = getCollectionDB(dbClient, 'curso', 'modulo');
     return await collection.insertOne(novoCurso);
 };
 
 const updateCourse = async (dbClient, id, atualizacao) => {
-    const collection = getCollectionDB(req.dbClient, 'curso', 'modulo');
+    const collection = getCollectionDB(dbClient, 'curso', 'modulo');
     return await collection.replaceOne({ _id: new ObjectId(id) }, atualizacao);
 };
 
 const patchCourse = async (dbClient, id, atualizacaoParcial) => {
-    const collection = getCollectionDB(req.dbClient, 'curso', 'modulo');
+    const collection = getCollectionDB(dbClient, 'curso', 'modulo');
     return await collection.updateOne({ _id: new ObjectId(id) }, { $set: atualizacaoParcial });
 };
 
 const deleteCourse = async (dbClient, id) => {
-    const collection = getCollectionDB(req.dbClient, 'curso', 'modulo');
+    const collection = getCollectionDB(dbClient, 'curso', 'modulo');
     return await collection.deleteOne({ _id: new ObjectId(id) });
 };
 
