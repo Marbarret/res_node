7a4b8accffd1463f7cc2188d5f49ea146bbce0de
Marcylene Barreto <marsouza93@gmail.com>
2025-01-14 20:50:18 -0300

    documentacao em openai 3.0

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const morgan = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const fs = require('fs');
+const yaml = require('yaml');
+
 const { connectToDatabase, client } = require('./src/data/db');
+const swaggerFile = fs.readFileSync('./swagger.yaml', 'utf8');
+const swaggerDocument = yaml.parse(swaggerFile);
 
 const userRoute = require('./src/routes/userRoute');
 const CustomError = require("./src/utils/CustomError");
@@ -30,9 +36,10 @@ app.use((req, res, next) => {
 
 app.use(dbMiddleware);
 
-app.use('/busease-api/v1/users', userRoute);
+app.use('/busease-api/v1/bse', userRoute);
 app.use('/busease-api/v1/auth', authRoute);
-app.use('/busease-api/v1/users', dependentRoute);
+app.use('/busease-api/v1/bse', dependentRoute);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res, next) => {
     const erro = new CustomError('Nenhuma rota encontrada', 404);
